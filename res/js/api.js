1f68b2efc8e5e1e8aa47069fb7874f0538391ce0
Karel Paan <paan.karel@gmail.com>
2020-10-21 19:19:49 +0300

    home2 task1

---
+++ res/js/api.js
@@ -0,0 +1,13 @@
+const api_url = 'https://private-anon-a152871d21-wad20postit.apiary-mock.com/';
+
+function Get(url) {
+    let settings = {
+        "url": api_url + url,
+        "method": "GET",
+    };
+    return $.ajax(settings).done((response) => {
+        return response;
+    }).fail((response) => {
+        console.log(response);
+    });
+};
