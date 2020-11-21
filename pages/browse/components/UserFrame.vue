87593febd1cc8140a95e986e3bf168ec48c49aa4
Tarmo Pungas <>
2020-11-21 14:50:25 +0200

    home-3 browse page now complete

--- pages/browse/components/UserFrame.vue
+++ pages/browse/components/UserFrame.vue
@@ -1,69 +1,93 @@
 <template>
   <div>
     <div class="avatar-container">
-      <img :src="authorAvatar" alt="profile">
-      <div style="margin-left: 1rem">{{ authorFirstname }} {{ authorLastname }}</div>
-      <div class="post-actions pl-3">
-        <button @click="followButtonPress" type="button" name="follow" :class="`follow-button btn btn-${pressed ? 'follow-button followed' : 'dark'}`">
-          {{text}}</button>
+      <img :src="authorAvatar" alt="profile" class="subscription-avatar">
+      <p class="author-name"> {{ authorFirstname }} {{ authorLastname }}</p>
+      <div class="post-actions">
+        <button @click="followButtonPress" type="button" name="follow"
+                :class="`follow-button btn btn-${pressed ? 'follow-button followed' : 'dark'}`">
+          {{text}}
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'UserFrame',
-  data() {
-    return {
-      text: "Follow",
-      pressed: false,
-    };
-  },
-  computed: {
-    authorFirstname() {
-      return this.user?.firstname || ''
+  export default {
+    name: 'UserFrame',
+    data() {
+      return {
+        text: "Follow",
+        pressed: false,
+      };
     },
-    authorLastname() {
-      return this.user?.lastname || ''
+    computed: {
+      authorFirstname() {
+        return this.user?.firstname || ''
+      },
+      authorLastname() {
+        return this.user?.lastname || ''
+      },
+      authorAvatar() {
+        return this.user?.avatar || ''
+      },
     },
-    authorAvatar() {
-      return this.user?.avatar || ''
+    methods: {
+      followButtonPress() {
+        this.pressed = !this.pressed;
+        if (this.pressed) {
+          this.text = "Followed"
+        } else {
+          this.text = "Follow"
+        }
+      },
     },
-  },
-  methods: {
-    followButtonPress() {
-      this.pressed = !this.pressed;
-      if (this.pressed) {
-        this.text = "Followed"
-      } else {
-        this.text = "Follow"
+    props: {
+      user: {
+        type: Object,
+        required: true,
       }
-    },
-  },
-  props: {
-    user: {
-      type: Object,
-      required: true,
     }
   }
-}
 </script>
 
 <style scoped>
+  .avatar-container {
+    border: #4d4d4d 0.1rem solid;
+    border-radius: 15px;
+    width: 75% !important;
+    height: 20rem !important;
+    padding-top: 1rem;
+    margin-top: 1rem;
+    text-align: center;
+  }
 
-.follow-button {
-  background-color: #8a8a8a;
-  width: 80px;
-  height: 25px;
-  line-height: 10px;
-  text-align: left;
-  border: none;
-}
+  .subscription-avatar {
+    width: 10rem !important;
+    height: 10rem !important;
+    object-fit: cover;
+    object-position: 50% 0;
+    border-radius: 50%;
+  }
 
-.follow-button.followed {
-  background-color: blueviolet;
-  color: white;
-}
+  .follow-button {
+    background-color: #8a8a8a;
+    width: 120px;
+    height: 45px;
+    border: none;
+  }
+
+  .follow-button.followed {
+    background-color: blueviolet;
+    color: white;
+  }
+
+  .author-name {
+    font-size: larger;
+    font-weight: bold;
+    margin-top: 1rem;
+    text-align: center;
+  }
 
 </style>
--- pages/browse/index.vue
+++ pages/browse/index.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="usersPanel">
-    <UserFrame v-for="user in profiles" :user="user" :key="user.id"/>
+    <UserFrame v-for="user in profiles" :user="user" :key="user.id" align="center"/>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
-import UserFrame from '@/pages/browse/components/UserFrame';
+  import {mapGetters} from 'vuex';
+  import UserFrame from '@/pages/browse/components/UserFrame';
 
-export default {
-  name: 'browse',
-  components: { UserFrame },
-  computed: {
-    ...mapGetters({
-      profiles: 'users/getProfiles'
-    })
-  },
-  mounted() {
-    this.$store.dispatch('users/fetchProfiles')
+  export default {
+    name: 'browse',
+    components: {UserFrame},
+    computed: {
+      ...mapGetters({
+        profiles: 'users/getProfiles'
+      })
+    },
+    mounted() {
+      this.$store.dispatch('users/fetchProfiles')
+    }
   }
-}
 </script>
 
 <style scoped>
-.usersPanel {
-  background-color: white;
-  place-content: center;
-}
+  .usersPanel {
+    background-color: white;
+    width: 75%;
+    min-height: 100%;
+    margin: auto auto;
+    padding: 30px 30px 15px 15px;
+  }
 </style>
