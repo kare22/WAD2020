03b5fc06a030d5247d05cb3ee6a7439b8b97bb15
karel22 <paan.karel@gmail.com>
2020-12-11 15:23:20 +0200

    home4 post posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authorize = require('../middlewares/authorize');
 const PostModel = require('../models/PostModel');
 
-
 router.get('/', authorize, (request, response) => {
 
     // Endpoint to get posts of people that currently logged in user follows or their own posts
@@ -23,8 +22,10 @@ router.get('/', authorize, (request, response) => {
 });
 
 router.post('/', authorize,  (request, response) => {
-
-    // Endpoint to create a new post
+    console.log('requst', request);
+    PostModel.create({ ...request.body, userId: request.currentUser.id }, (data) => {
+        response.status(201).json(data);
+    });
 
 });
 
